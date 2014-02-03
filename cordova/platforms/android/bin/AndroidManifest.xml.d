53fdfaca2fe0b95d255dc63e3d0f2e0e8ca0bc1f
Christoffer Rosen <cbr4830@rit.edu>
2014-02-03 13:48:41 -0500

    Removing cordova from the repo folder.

--- cordova/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/bin/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/AndroidManifest.xml \
--- cordova/platforms/android/bin/TourTrak-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/bin/TourTrak-debug-unaligned.apk : \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/bin/TourTrak.ap_ \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/bin/classes.dex \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/libs/gson-2.2.2.jar \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/libs/cordova-3.1.0.jar \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/libs/gcm.jar \
-/Applications/XAMPP/xamppfiles/htdocs/devcycle-app/cordova/platforms/android/libs/android-support-v4.jar \
