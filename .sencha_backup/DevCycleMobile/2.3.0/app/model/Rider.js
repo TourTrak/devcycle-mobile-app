20fb47e831a90ddbb7e43a391d362a6d0a29644e
jeklund <eklund.joshua@gmail.com>
2014-09-21 22:26:51 -0400

    Removed all .sencha_backup files that were accidentally added in the Feature branch before branching to the Group branch. Also added these files to the .gitignore

--- .sencha_backup/DevCycleMobile/2.3.0/app/model/Rider.js
+++
@@ -1,9 +0,0 @@
-Ext.define('DevCycleMobile.model.Rider', {
-	extend: 'Ext.data.Model',
-	config: {
-		fields: [
-			'riderId',
-			'prod' 	// this is a flag added to show we got this rider id from production server -> old beta riders were registered to the beta servers
-		],
-	}
-});
