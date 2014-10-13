166dcd7af626cc4670d9ad95a98585ce9feec507
StephanJW <stephanjwlodarczyk@gmail.com>
2014-10-13 19:46:44 -0400

    Added AJAX functionality for joining and creating groups

--- app/controller/Groups.js
+++ app/controller/Groups.js
@@ -32,35 +32,66 @@ Ext.define('DevCycleMobile.controller.Groups', {
 		
 	// Adds user to specified group
 	joinGroup: function() {
-		var code = Ext.getCmp('join_group_code').getValue();
-		if(code != '' && code.length >=CODE_MIN && code.length <=CODE_MAX)
+		var groupCode = Ext.getCmp('join_group_code').getValue();
+		if(groupCode != '' && groupCode.length >=CODE_MIN && groupCode.length <=CODE_MAX)
 		{
-			// Do search on group
+			Ext.Ajax.request({
+				url: this.tourInfo.data.dcs_url + '/join_group/',
+				method: 'POST',
+				scope: this,
+				params: {				
+					riderId: this.riderInfo.get('riderId')
+					code: groupCode
+				},
+				success: function(response){
+					alert('Joined group successfully!');
+				}
 		}
 		else {
 			alert('Error: The group does not exist.');
 		}
 	},
 	
-	// Creates custom group provided by user
-	createGroup: function(groupName) {
-		var name = Ext.getCmp('group_name').getValue();
-		var code = Ext.getCmp('create_group_code').getValue();
-		if(name != '' && name.length <= NAME_MAX) {
-			if(code == '') {
-				// generate random code and add group to database				
-				Ext.getCmp('load-indicator').show();
+	// Handles creating a group based on provided user input, sends it off to postGroup 
+	createGroup: function() {
+		Ext.getCmp('load-indicator').show();	
+		var groupName = Ext.getCmp('group_name').getValue();
+		var groupCode = Ext.getCmp('create_group_code').getValue();
+		var canCreateGroup = false;
+		if(groupName != '' && groupName.length <= NAME_MAX) {
+			if(groupCode == '') {
+				// generate random code
+				groupCode = Math.random().toString(36).slice(2);
+				alert('Group Code: ' + groupCode);
+				canCreateGroup = true;
 			}
 			else if(code.length >= CODE_MIN && code.length <= CODE_MAX) {
-				// add group to database
+				canCreateGroup = true;
 			}
 			else {
 				alert('Error: Customized group code must be between 3 to 7 characters');
 			}
+			
+			if (canCreateGroup)
+			{
+				Ext.Ajax.request({
+				url: this.tourInfo.data.dcs_url + '/create_group/',// need to define
+				method: 'POST',
+				scope: this,
+				params: {				
+					riderId: this.riderInfo.get('riderId')
+					group: groupName,
+					code: code
+				},
+				success: function(response){
+					alert('Group successfully created!');
+				}			
+			}			
 		}
 		else {
 			alert('Error: Please enter a group name (MAX: 30 characters)');
 		}
+		Ext.getCmp('load-indicator').hide();
 	},
 		
 	// Removes user from specified group 
