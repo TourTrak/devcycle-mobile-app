246674f164369e554fba972b51673fb24ff03cc0
Conor Wright <conor13@msn.com>
2015-10-29 00:04:56 -0400

    updated windows plugin fetcher to reflect new repos

--- fetchPluginsWindows.py
+++ fetchPluginsWindows.py
@@ -5,6 +5,7 @@ Requires sudo access. Mac/Linux support only
 
 Usage: sudo python fetchPlugins.py
 Author: Christoffer Rosen <cbr4830@rit.edu>
+Editted By: Conor Wright <cgw5994@rit.edu>
 '''
 
 import os
@@ -13,20 +14,16 @@ import os
 os.chdir("cordova")
 
 # Add the cordova device plugin
-os.system("cordova -d plugin add https://github.com/apache/cordova-plugin-device.git")
-
-# Add the cordova push notification plugin
-#os.system("cordova -d plugin add https://github.com/phonegap-build/PushPlugin")
+os.system("cordova plugin add https://github.com/apache/cordova-plugin-device.git")
 
 # Add the tourtrak iOS plugin
-os.system("cordova -d plugin add https://github.com/LeeFischman/tourtrak-ios-plugin.git")
-# Original os.system("cordova -d plugin add https://github.com/cck9672/geolocation-ios-noapp.git")
+os.system("cordova plugin add https://github.com/TourTrak/tourtrak-ios-plugin.git")
 
 # Add the tourtrak android plugin
-os.system("cordova -d plugin add https://github.com/tofferrosen/tourtrak-android-plugin.git")
+os.system("cordova plugin add https://github.com/TourTrak/tourtrak-android-plugin.git")
 
 # Add the cordova geolocation plugin
-os.system("cordova -d plugin add https://github.com/apache/cordova-plugin-geolocation.git")
+os.system("cordova plugin add https://github.com/apache/cordova-plugin-geolocation.git")
 
 # Print out all the plugins installed for the user
-os.system("cordova -d plugin ls")
+os.system("cordova plugin ls")
